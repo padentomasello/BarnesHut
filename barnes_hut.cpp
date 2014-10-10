9fc9c7b5c1571412626c22c75b606ef26a593a8d
Paden Tomasello <padent@berkeley.edu>
2014-10-09 23:16:09 -0700

    Refactoring

--- barnes_hut.cpp
+++ barnes_hut.cpp
@@ -27,7 +27,8 @@
 #define MAXDEPTH 32
 
 struct KernelArgs{
-  cl_mem posxl, posyl, poszl, childl, massl, startl, minx_d, maxx_d, miny_d, maxy_d, minz_d, maxz_d, blocked, step_d, bottom_d, max_depth_d, radius_d;
+  cl_mem posx, posy, posz, child, mass, start, minx, maxx, miny, maxy, minz, maxz, blocked, step, bottom, max_depth, radius, count;
+  cl_mem velx, vely, velz, accx, accy, accz, sort;
   int num_bodies, num_nodes;
   int num_args;
 };
@@ -37,43 +38,100 @@ struct HostMemory {
   int* start;
 };
 
+void CreateMemBuffer (cl_vars_t* cv, KernelArgs* args, HostMemory* host_memory) {
+  cl_int err;
+  args->step = clCreateBuffer(cv->context, CL_MEM_READ_WRITE,
+      sizeof(int) * 1, NULL, &err);
+  args->bottom = clCreateBuffer(cv->context, CL_MEM_READ_WRITE,
+      sizeof(int) * 1, NULL, &err);
+  args->max_depth = clCreateBuffer(cv->context, CL_MEM_READ_WRITE,
+      sizeof(int) * 1, NULL, &err);
+  args->radius = clCreateBuffer(cv->context, CL_MEM_READ_WRITE,
+      sizeof(float) * 1, NULL, &err);
+  // Create Buffers  NOTE* These do need to be (num_nodes + 1)
+
+  args->posx = clCreateBuffer(cv->context, CL_MEM_COPY_HOST_PTR | CL_MEM_READ_WRITE,
+      sizeof(float) * (num_nodes + 1), host_memory->posx, &err);
+  CHK_ERR(err);
+  args->posz = clCreateBuffer(cv->context, CL_MEM_COPY_HOST_PTR | CL_MEM_READ_WRITE,
+      sizeof(float)*(num_nodes + 1), host_memory.posy, &err);
+  args->posy = clCreateBuffer(cv.context, CL_MEM_COPY_HOST_PTR | CL_MEM_READ_WRITE,
+      sizeof(float)*(num_nodes + 1), host_memory->posz, &err);
+  CHK_ERR(err);
+  args->mass = clCreateBuffer(cv->context, CL_MEM_READ_WRITE,
+      sizeof(float)*(num_nodes + 1), NULL, &err);
+  CHK_ERR(err);
+  args->count = clCreateBuffer(cv->context, CL_MEM_READ_WRITE,
+      sizeof(float)*(num_nodes + 1), NULL, &err);
+  CHK_ERR(err);
+  args->start = clCreateBuffer(cv->context, CL_MEM_READ_WRITE,
+      sizeof(int)*(num_nodes + 1), NULL, &err);
+  CHK_ERR(err);
+  args->child = clCreateBuffer(cv->context, CL_MEM_READ_WRITE,
+      sizeof(float)*8*(num_nodes + 1), NULL, &err);
+  //Set the following alligned on WARP boundaries
+  int inc = (num_bodies + WARPSIZE -1) & (-WARPSIZE);
+    cl_buffer_region velxl_region = {0, 1*inc};
+    cl_buffer_region velyl_region = {1*inc, 2*inc};
+    cl_buffer_region velzl_region = {2*inc, 3*inc};
+    cl_buffer_region accxl_region = {3*inc, 4*inc};
+    cl_buffer_region accyl_region = {4*inc, 5*inc};
+    cl_buffer_region acczl_region = {5*inc, 6*inc};
+    cl_buffer_region sortl_region = {6*inc, 7*inc};
+    args->velx = clCreateSubBuffer(args->child, CL_MEM_READ_WRITE,
+        CL_BUFFER_CREATE_TYPE_REGION, &velxl_region, &err);
+    args->vely = clCreateSubBuffer(args->child, CL_MEM_READ_WRITE,
+        CL_BUFFER_CREATE_TYPE_REGION, &velyl_region, &err);
+    args->velz = clCreateSubBuffer(args->child, CL_MEM_READ_WRITE,
+        CL_BUFFER_CREATE_TYPE_REGION, &velzl_region, &err);
+    args->accx = clCreateSubBuffer(args->child, CL_MEM_READ_WRITE,
+        CL_BUFFER_CREATE_TYPE_REGION, &accxl_region, &err);
+    args->accy = clCreateSubBuffer(args->child, CL_MEM_READ_WRITE,
+        CL_BUFFER_CREATE_TYPE_REGION, &accyl_region, &err);
+    args->accz = clCreateSubBuffer(args->child, CL_MEM_READ_WRITE,
+        CL_BUFFER_CREATE_TYPE_REGION, &acczl_region, &err);
+    args->sort = clCreateSubBuffer(args->child, CL_MEM_READ_WRITE,
+        CL_BUFFER_CREATE_TYPE_REGION, &sortl_region, &err);
+
+}
+
 
 void SetArgs(cl_kernel *kernel, KernelArgs* args){
   // Allocate device memory // TODO Can change all host arrays to size num_cells. Using num_boides for debugging purpuses.
   cl_int err;
-  err = clSetKernelArg(*kernel, 0, sizeof(cl_mem), &args->posxl);
+  err = clSetKernelArg(*kernel, 0, sizeof(cl_mem), &args->posx);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 1, sizeof(cl_mem), &args->posyl);
+  err = clSetKernelArg(*kernel, 1, sizeof(cl_mem), &args->posy);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 2, sizeof(cl_mem), &args->poszl);
+  err = clSetKernelArg(*kernel, 2, sizeof(cl_mem), &args->posz);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 3, sizeof(cl_mem), &args->childl);
+  err = clSetKernelArg(*kernel, 3, sizeof(cl_mem), &args->child);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 4, sizeof(cl_mem), &args->massl);
+  err = clSetKernelArg(*kernel, 4, sizeof(cl_mem), &args->mass);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 5, sizeof(cl_mem), &args->startl);
+  err = clSetKernelArg(*kernel, 5, sizeof(cl_mem), &args->start);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 6, sizeof(cl_mem), &args->minx_d);
+  err = clSetKernelArg(*kernel, 6, sizeof(cl_mem), &args->minx);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 7, sizeof(cl_mem), &args->maxx_d);
+  err = clSetKernelArg(*kernel, 7, sizeof(cl_mem), &args->maxx);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 8, sizeof(cl_mem), &args->miny_d);
+  err = clSetKernelArg(*kernel, 8, sizeof(cl_mem), &args->miny);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 9, sizeof(cl_mem), &args->maxy_d);
+  err = clSetKernelArg(*kernel, 9, sizeof(cl_mem), &args->maxy);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 10, sizeof(cl_mem), &args->minz_d);
+  err = clSetKernelArg(*kernel, 10, sizeof(cl_mem), &args->minz);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 11, sizeof(cl_mem), &args->maxz_d);
+  err = clSetKernelArg(*kernel, 11, sizeof(cl_mem), &args->maxz);
   CHK_ERR(err);
   err = clSetKernelArg(*kernel, 12, sizeof(cl_mem), &args->blocked);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 13, sizeof(cl_mem), &args->step_d);
+  err = clSetKernelArg(*kernel, 13, sizeof(cl_mem), &args->step);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 14, sizeof(cl_mem), &args->bottom_d);
+  err = clSetKernelArg(*kernel, 14, sizeof(cl_mem), &args->bottom);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 15, sizeof(cl_mem), &args->max_depth_d);
+  err = clSetKernelArg(*kernel, 15, sizeof(cl_mem), &args->max_depth);
   CHK_ERR(err);
-  err = clSetKernelArg(*kernel, 16, sizeof(cl_mem), &args->radius_d);
+  err = clSetKernelArg(*kernel, 16, sizeof(cl_mem), &args->radius);
   CHK_ERR(err);
   err = clSetKernelArg(*kernel, 17, sizeof(int), &args->num_bodies);
   CHK_ERR(err);
@@ -164,59 +222,6 @@ int main (int argc, char *argv[])
   // Create Scalar buffers?
   cl_int err = CL_SUCCESS;
 
-  args.step_d = clCreateBuffer(cv.context, CL_MEM_READ_WRITE,
-      sizeof(int) * 1, NULL, &err);
-  args.bottom_d = clCreateBuffer(cv.context, CL_MEM_READ_WRITE,
-      sizeof(int) * 1, NULL, &err);
-  args.max_depth_d = clCreateBuffer(cv.context, CL_MEM_READ_WRITE,
-      sizeof(int) * 1, NULL, &err);
-  args.radius_d = clCreateBuffer(cv.context, CL_MEM_READ_WRITE,
-      sizeof(float) * 1, NULL, &err);
-  // Create Buffers  NOTE* These do need to be (num_nodes + 1)
-
-
-  args.posxl = clCreateBuffer(cv.context, CL_MEM_COPY_HOST_PTR | CL_MEM_READ_WRITE,
-      sizeof(float) * (num_nodes + 1), host_memory.posx, &err);
-  CHK_ERR(err);
-  args.poszl = clCreateBuffer(cv.context, CL_MEM_COPY_HOST_PTR | CL_MEM_READ_WRITE,
-      sizeof(float)*(num_nodes + 1), host_memory.posy, &err);
-  args.posyl = clCreateBuffer(cv.context, CL_MEM_COPY_HOST_PTR | CL_MEM_READ_WRITE,
-      sizeof(float)*(num_nodes + 1), host_memory.posz, &err);
-  CHK_ERR(err);
-  args.massl = clCreateBuffer(cv.context, CL_MEM_READ_WRITE,
-      sizeof(float)*(num_nodes + 1), NULL, &err);
-  CHK_ERR(err);
-  countl = clCreateBuffer(cv.context, CL_MEM_READ_WRITE,
-      sizeof(float)*(num_nodes + 1), NULL, &err);
-  CHK_ERR(err);
-  args.startl = clCreateBuffer(cv.context, CL_MEM_READ_WRITE,
-      sizeof(int)*(num_nodes + 1), NULL, &err);
-  CHK_ERR(err);
-  args.childl = clCreateBuffer(cv.context, CL_MEM_READ_WRITE,
-      sizeof(float)*8*(num_nodes + 1), NULL, &err);
-  //Set the following alligned on WARP boundaries
-  int inc = (num_bodies + WARPSIZE -1) & (-WARPSIZE);
-    cl_buffer_region velxl_region = {0, 1*inc};
-    cl_buffer_region velyl_region = {1*inc, 2*inc};
-    cl_buffer_region velzl_region = {2*inc, 3*inc};
-    cl_buffer_region accxl_region = {3*inc, 4*inc};
-    cl_buffer_region accyl_region = {4*inc, 5*inc};
-    cl_buffer_region acczl_region = {5*inc, 6*inc};
-    cl_buffer_region sortl_region = {6*inc, 7*inc};
-    //velxl = clCreateSubBuffer(args.childl, CL_MEM_READ_WRITE,
-        //CL_BUFFER_CREATE_TYPE_REGION, &velxl_region, &err);
-    //velyl = clCreateSubBuffer(args.childl, CL_MEM_READ_WRITE,
-        //CL_BUFFER_CREATE_TYPE_REGION, &velyl_region, &err);
-    //velzl = clCreateSubBuffer(args.childl, CL_MEM_READ_WRITE,
-        //CL_BUFFER_CREATE_TYPE_REGION, &velzl_region, &err);
-    //accxl = clCreateSubBuffer(args.childl, CL_MEM_READ_WRITE,
-        //CL_BUFFER_CREATE_TYPE_REGION, &accxl_region, &err);
-    //accyl = clCreateSubBuffer(args.childl, CL_MEM_READ_WRITE,
-        //CL_BUFFER_CREATE_TYPE_REGION, &accyl_region, &err);
-    //acczl = clCreateSubBuffer(args.childl, CL_MEM_READ_WRITE,
-        //CL_BUFFER_CREATE_TYPE_REGION, &acczl_region, &err);
-    //sortl = clCreateSubBuffer(args.childl, CL_MEM_READ_WRITE,
-        //CL_BUFFER_CREATE_TYPE_REGION, &sortl_region, &err);
 
 
   /* Set local work size and global work sizes */
